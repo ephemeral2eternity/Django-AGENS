66c5795f07114ae6e91dedd4de6283b1657ac0bb
Chen Wang <chenw@andrew.cmu.edu>
2015-02-16 13:17:00 +0000

    video add operation done

--- video/views.py
+++ video/views.py
@@ -25,7 +25,7 @@ def initVideo(request):
 	for vid in cached_videos:
 		vid_id = vid
 		vid_name = random.choice(real_cached_videos)
-		vid_srvs = hostname + ','
+		vid_srvs = hostname + ', '
 		# print("Is ", srv, " the localhost? ", isLocal)
 		cur_vid = Video(id=vid_id, name=vid_name, srvs=vid_srvs)
 		cur_vid.save()
@@ -52,20 +52,29 @@ def add(request):
 		for srv in srvs:
 			vid_list_str = request.POST.get(srv, "")
 			vid_list = [int(s) for s in vid_list_str.split(',')]
-			print(vid_list)
-			for vid in vidlist:
+			# print(vid_list)
+			for vid in vid_list:
 				vid_id = vid
-				vid_obj = Video.objects.get(id=vid_id)
-				if not vid_obj:
-					vid_obj.srvs = vid_obj.srvs + srv + ','
+				# print('Processing video ', vid_id, "!")
+				vid_exist = Video.objects.filter(id=vid_id)
+				if vid_exist.count() > 0:
+					vid_obj = Video.objects.get(id=vid_id)
+					# print("Video object with id = ", vid_id, " found in local cache table!")
+					vid_srv_list = vid_obj.srvs.split(', ')
+					# print("Video server list in cache table:", vid_srv_list)
+					if srv not in vid_srv_list:
+						vid_obj.srvs = vid_obj.srvs + srv + ', '
+						# print("New srvs for video ", vid, " is ", vid_obj.srvs)
+						vid_obj.save()
+					# print("Server ", srv, " has been added to Video ", vid, " in cache table!")
 				else:
 					vid_name = 'BBB'
 					isLocal = False
-					vid_srvs = srv + ','
-					new_vid = Video(id=vid_id, name=vid_name, isLocal=isLocal, srvs=vid_srvs)
-					new_vid.save()
-				print("Video " + vid + " on server " + srv + " has been updated!")
+					vid_srvs = srv + ', '
+					# print("srvs for video ", vid, " is ", vid_srvs)
+					new_vid_obj = Video(id=vid_id, name=vid_name, isLocal=isLocal, srvs=vid_srvs)
+					new_vid_obj.save()
+					# print("Video ", vid, " on server ", srv," has been added to cache table!")
 		return HttpResponse("Successfully update video list cached on " + srv + "!")
 	elif request.method == "GET":
 		return HttpResponse("You should use POST method when calling http://cache_agent:port/video/add/ to add video lists!")
-				
